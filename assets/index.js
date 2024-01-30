d5cb4e9fb41854a1f4a4dd2c6886e08f9e082d68
Collince Owinoh <coowinoh@gmail.com>
2024-01-30 14:59:38 +0300

    portfolio update

--- assets/index.js
+++ assets/index.js
@@ -1,16 +1,21 @@
 function sendEmail() {
-
-    Email.send({
-        Host : "smtp.elasticmail.com",
-        Username : "coowinoh@gmail.com",
-        Password : "mK8oGS9kIH90-_y",
-        To : 'coowinoh1@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Inquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
+  Email.send({
+      Host: "smtp.gmail.com",
+      Port: "587 (TLS) or 465 (SSL)",
+      Username: "coowinoh@gmail.com",
+      Password: "MyMother324@!",
+      To: 'coowinoh1@gmail.com',
+      From: document.getElementById("email").value,
+      Subject: "New Contact Form Inquiry",
+      Body: "Name: " + document.getElementById("name").value +
+            "\nEmail: " + document.getElementById("email").value +
+            "\nPhone Number: " + document.getElementById("phoneNumber").value +
+            "\nMessage: " + document.getElementById("message").value
+  }).then(
+      message => {
+          alert("Message sent successfully!");
+          // You can add any additional logic here, such as clearing the form fields
+          document.getElementById("contact-page").reset();
+      }
+  );
 }
-
-
